65ea99e6c78fd9350a2f3e96a311b740e306111b
Arin Kumar Joshi <110386547+arinkumarjoshi@users.noreply.github.com>
2023-04-02 14:55:26 +0530

    Add files via upload

---
+++ addtwonum main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class addtwonum main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a;
+        int b;
+        int sum = a+b;
+        string sum = sc.nextLine();
+        System.out.print(sum);
+
+    }
+    
+}
