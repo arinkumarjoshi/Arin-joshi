583cc095462273502be599486f02f79d8189d98f
Arin Kumar Joshi <110386547+arinkumarjoshi@users.noreply.github.com>
2023-04-02 14:57:49 +0530

    Create calculation.java
    calculate a operation

---
+++ calculation.java
@@ -0,0 +1,19 @@
+public class calculation {
+    public  static void main (String[] args)
+     {
+      String name = "dikesh joshi";
+      //System.out.println(name);
+       int a = 10;
+      int b = 5;
+      int sum = a+b;
+      //System.out.println(sum);
+      int diff = b-a;
+     // System.out.println(diff);
+        int mul = a*b;
+       // System.out.println(mul);
+       int ans = (a*b) / (a-b) ;
+       System.out.println(ans);
+
+    }
+    
+}
