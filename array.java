b9e493b7d56c6f29acaf6c742e602740e0a5981f
Arin Kumar Joshi <110386547+arinkumarjoshi@users.noreply.github.com>
2023-04-11 16:14:28 +0530

    Create array.java
    Signed-off-by: Arin Kumar Joshi <110386547+arinkumarjoshi@users.noreply.github.com>

---
+++ array.java
@@ -0,0 +1,13 @@
+
+import java.util.*;
+public class array {
+    public static void main (String[] args){
+        int [] num = new int[3];
+        num[0] = 98; //dbms
+        num[1] = 95; //math
+        num[2] = 99; //cao
+        System.out.println(num[0]);
+        System.out.println(num[1]);
+        System.out.println(num[2]);
+    }
+}
