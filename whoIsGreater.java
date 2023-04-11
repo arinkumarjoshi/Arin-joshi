fea3a440ac26a65598c4b4ef885b14eb7ababbc7
Arin Kumar Joshi <110386547+arinkumarjoshi@users.noreply.github.com>
2023-04-11 16:12:38 +0530

    Create whoIsGreater.java
    Signed-off-by: Arin Kumar Joshi <110386547+arinkumarjoshi@users.noreply.github.com>

---
+++ whoIsGreater.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class whoIsGreater {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+          
+        if(a>b){
+            System.out.println("the Greator is a"+"="+ a);
+        }
+        else{
+            System.out.println("the greator is b"+"="+ b);
+        }
+    }
+    
+}
