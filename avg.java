bcf454856dbd58181c894fbe9411137c741766c5
Arin Kumar Joshi <110386547+arinkumarjoshi@users.noreply.github.com>
2023-04-11 16:11:05 +0530

    Create avg.java
    Signed-off-by: Arin Kumar Joshi <110386547+arinkumarjoshi@users.noreply.github.com>

---
+++ avg.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+import javax.sql.rowset.spi.SyncResolver;
+
+import java.util.*;
+public class avg {
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int c = sc.nextInt();
+    
+   int  avarage = (a+b+c) / 3;
+   System.out.println(avarage);
+   
+   
+    
+    }
+    
+}
