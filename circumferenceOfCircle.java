bb57050aa0daa18bc1476eec38b1381f870db937
Arin Kumar Joshi <110386547+arinkumarjoshi@users.noreply.github.com>
2023-04-11 16:13:14 +0530

    Create circumferenceOfCircle.java
    Signed-off-by: Arin Kumar Joshi <110386547+arinkumarjoshi@users.noreply.github.com>

---
+++ circumferenceOfCircle.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class circumferenceOfCircle {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        int r = sc.nextInt();
+        int x = (22)/7;
+        int circumference;
+
+        circumference = r*x*2;
+        System.out.println("the value of circumference"+ "=" + circumference);
+    }
+    
+}
+h
